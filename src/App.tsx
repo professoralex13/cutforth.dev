c530b710f4fe4aea80e8ad7de210d3ef8526f079
Alex Cutforth <alex@densem.net>
2026-07-09 13:00:02 +1200

    setup biome

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 function App() {
-  return (
-    <>
-      <h1 className="text-2xl">Hello</h1>
-    </>
-  );
+	return (
+		<>
+			<h1 className="text-2xl">Hello</h1>
+		</>
+	);
 }
 
 export default App;
